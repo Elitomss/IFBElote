841899c81afa6184ecb4e302d0916597eb5290d0
Elitomss <66841557+Elitomss@users.noreply.github.com>
2020-06-12 22:46:01 +0200

    Add files via upload

--- jeu.h
+++ jeu.h
@@ -1,1 +1,11 @@
+void CorrespondanceInt(int Chiffre);
 
+void Distribution(int *PaquetDeCartes, int *MainJoueur, int *MainOuest, int *MainEst, int *MainNord);
+void MelangeCartes(int *PaquetDeCartes);
+
+int ComptagePoints(int *Recup, int Atout);
+int GagnantPli(int *Carte, int Atout);
+int MancheGagneePar(int VainqueurManche, int Compteur, int *Pli, int *RecupJoueur, int *RecupOuest, int *RecupEst, int *RecupNord);
+
+void EnteteJeu(int CouleurAtout, int PhaseDeJeu);
+void AffichageCartes(int *MainJoueur);
